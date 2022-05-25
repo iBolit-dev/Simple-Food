654d539ea2ccb52db9bd0c7fe1f16221fb93ee3d
iBolit <zhenja-kokhtenko@rambler.ru>
2022-05-25 18:54:02 +0300

    fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -26,4 +26,31 @@ $(function () {
     }
 
   });
-})
+
+  // я чисто случайно нашёл как сделать это меню поиска, чтобы работало как надо, просто подставил свои классы, с тем, что ты мне кидал, у меня не получилось сделать, если тебе не сложно объясни плиз, что я здесь прописал и что за что отвечает, потому что я сам вообще хз, кроме строки classList.toggle, как я понимаю, это то же самое, что и toggleClass только написано на чистом js
+
+
+  let btn = document.querySelector('.users__form-button');
+  let users = document.querySelector('.users__form');
+
+  const toggleMenu = () => {
+    users.classList.toggle('search-active');
+  }
+
+  btn.addEventListener('click', e => {
+    e.stopPropagation();
+
+    toggleMenu();
+  });
+
+  document.addEventListener('click', e => {
+    let target = e.target;
+    let its_users = target == users || users.contains(target);
+    let its_btn = target == btn;
+    let users_is_active = users.classList.contains('search-active');
+
+    if (!its_users && !its_btn && users_is_active) {
+      toggleMenu();
+    }
+  })
+});
